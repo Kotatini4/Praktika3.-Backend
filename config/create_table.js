d6cf97e3eca94bd82bcf040976b675f13511f1ab
Kotatini4 <kotatini4@gmail.com>
2025-04-09 11:16:58 +0300

    zarabotalo v pgAdmin

---
+++ config/create_table.js
@@ -0,0 +1,14 @@
+const db = require("./database");
+const models = require("../models");
+
+const sync = async () => await db.sync({ force: true });
+sync().then(() => {
+    models.Role.create({ name: "admin" });
+    models.Role.create({ name: "user" });
+    models.User.create({
+        email: "test@test.com",
+        password: "123456",
+        username: "admin",
+        role_id: 1,
+    });
+});
